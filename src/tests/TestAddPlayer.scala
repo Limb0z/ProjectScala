82035c3a2fdbb4fc25b3c49f07d98bf60ec57816
justinlim <justin.lim3411@gmail.com>
2019-04-04 16:27:30 -0400

    fixed tests

--- src/tests/TestAddPlayer.scala
+++ src/tests/TestAddPlayer.scala
@@ -1,5 +1,42 @@
 package tests
 
-class TestAddPlayer {
+import org.scalatest.FunSuite
+import gameFunctions.functions
+import gameObjects._
+import model.Player
 
+import scala.collection.mutable.ListBuffer
+
+class TestAddPlayer extends FunSuite {
+
+  def addPlayers(player:Player, world:World):Unit = {
+    world.playerList = world.playerList :+ player
+  }
+
+  test("add player") {
+    var p1:Player = new Player(Map("bandage" -> 0, "food" -> 0), 100.0, "alive", 100, 100)
+    var p2:Player = new Player(Map("bandage" -> 1, "food" -> 2), 100.0, "alive", 14, 17)
+    var p3:Player = new Player(Map("bandage" -> 6, "food" -> 9), 100.0, "alive", 91, 3)
+    var p4:Player = new Player(Map("bandage" -> 5, "food" -> 2), 100.0, "alive", 34, 84)
+    var p5:Player = new Player(Map("bandage" -> 3, "food" -> 4), 100.0, "alive", 2, 372)
+    var p6:Player = new Player(Map("bandage" -> 7, "food" -> 8), 100.0, "alive", 352, 869)
+
+    val l1:ListBuffer[Player] = new ListBuffer[Player]
+    val w1:World = new World
+    w1.playerList = l1
+
+    addPlayers(p1, w1)
+    addPlayers(p2, w1)
+    addPlayers(p3, w1)
+    addPlayers(p4, w1)
+    addPlayers(p5, w1)
+
+    assert(w1.playerList.contains(p1), true)
+    assert(w1.playerList.contains(p2), true)
+    assert(w1.playerList.contains(p3), true)
+    assert(w1.playerList.contains(p4), true)
+    assert(w1.playerList.contains(p5), true)
+    assert(w1.playerList.contains(p6), false)
+
+  }
 }
--- src/tests/TestPickUpItem.scala
+++ src/tests/TestPickUpItem.scala
@@ -1,15 +1,27 @@
 package tests
 
 import gameFunctions._
-import gameObjects.World
+import gameObjects._
 import org.scalatest.FunSuite
 import scala.collection.mutable.ListBuffer
 import model.Player
 
 class TestPickUpItem extends FunSuite {
 
-  def addPlayers(player:Player, world:World):Unit = {
-    world.playerList = world.playerList :+ player
+  def pickUpItem(player:Player, world:World): Unit = {
+    for (currentItem <- world.itemList){
+      if (player.locationX == currentItem.locationX && player.locationY == currentItem.locationY){
+        if (currentItem.isInstanceOf[bandage]){
+          player.backpack("bandage") += 1
+        }
+        if (currentItem.isInstanceOf[food]){
+          player.backpack("food") += 1
+        }
+        if (currentItem.isInstanceOf[ammo]){
+          player.backpack("ammo") += 1
+        }
+      }
+    }
   }
 
   test("pick up items and put it into backpack"){
@@ -20,16 +32,39 @@ class TestPickUpItem extends FunSuite {
     var p5:Player = new Player(Map("bandage" -> 3, "food" -> 4), 100.0, "alive", 2, 372)
     var p6:Player = new Player(Map("bandage" -> 7, "food" -> 8), 100.0, "alive", 352, 869)
 
-    var w1:World = new World
+    val i1:bandage = new bandage(100, 100)
+    val i2:bandage = new bandage(234, 567)
+    val i3:bandage = new bandage(0, 0)
+    val i4:food = new food(2, 372)
+    val i5:food = new food(1000, 1000)
+    val i6:food = new food(134, 987)
 
-    addPlayers(p1, w1)
-    addPlayers(p2, w1)
-    addPlayers(p3, w1)
-    addPlayers(p4, w1)
-    addPlayers(p5, w1)
+    val l1:ListBuffer[items] = ListBuffer(i1, i3, i5)
+    val l2:ListBuffer[items] = ListBuffer(i2, i4, i6)
+    val l3:ListBuffer[items] = ListBuffer(i1, i2, i3)
+    val l4:ListBuffer[items] = ListBuffer(i4, i5, i6)
 
-    println(w1)
+    val w1:World = new World
+    w1.itemList = l1
+    val w2:World = new World
+    w2.itemList = l2
+    val w3:World = new World
+    w3.itemList = l3
+    val w4:World = new World
+    w4.itemList = l4
 
-    assert(w1.contains(p1), true)
+    pickUpItem(p1, w1)
+    pickUpItem(p2, w2)
+    pickUpItem(p3, w3)
+    pickUpItem(p4, w4)
+    pickUpItem(p5, w4)
+    pickUpItem(p6, w3)
+
+    assert(p1.backpack("bandage") == 1,true)
+    assert(p2.backpack("bandage") == 1,true)
+    assert(p3.backpack("bandage") == 6,true)
+    assert(p4.backpack("food") == 2,true)
+    assert(p5.backpack("food") == 5,true)
+    assert(p6.backpack("food") == 8,true)
   }
 }
