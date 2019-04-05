79e5bcef7b4d698145b639d531332eccafe0cae7
gifd <kevin.huang17@yahoo.com>
2019-04-05 00:08:27 -0400

    major changes

---
+++ src/gameObjects/bullet.scala
@@ -0,0 +1,5 @@
+package gameObjects
+
+class bullet(var speed: Double, var x: Double, var y: Double, var eX: Double, var eY: Double, var velocityX: Double, var velocityY: Double, var damage: Double, var whoShot: String) {
+
+}
